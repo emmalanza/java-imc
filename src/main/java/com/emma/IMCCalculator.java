6c4889e1096ec373e90a3b0416635200073024b2
Emma <emmalanza@hotmail.com>
2025-02-17 10:46:37 +0100

    implement get imc classification method for passing test

--- src/main/java/com/emma/IMCCalculator.java
+++ src/main/java/com/emma/IMCCalculator.java
@@ -17,5 +17,25 @@ public class IMCCalculator {
         return Math.round(weight / (Math.pow(height, 2)) * 100) / 100.0;
     }
 
+    public String getIMCClassification() {
+        double imc = calculateIMC();
 
+        if (imc < 16) {
+            return "Delgadez severa";
+        } else if (imc >= 16 && imc < 17) {
+            return "Delgadez moderada";
+        } else if (imc >= 17 && imc < 18.5) {
+            return "Delgadez leve";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            return "Sobrepeso";
+        } else if (imc >= 30 && imc < 35) {
+            return "Obesidad leve";
+        } else if(imc >= 35 && imc < 40) {
+            return "Obesidad moderada";
+        } else {
+            return "Obesidad mórbida";
+        }
+    }
 }
