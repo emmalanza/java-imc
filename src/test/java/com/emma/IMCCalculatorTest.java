b06434251ea1853e4d85a347cfd634f4d56220f5
Emma <emmalanza@hotmail.com>
2025-02-17 10:44:17 +0100

    implement test for get imc classification - failed

--- src/test/java/com/emma/IMCCalculatorTest.java
+++ src/test/java/com/emma/IMCCalculatorTest.java
@@ -11,4 +11,24 @@ public class IMCCalculatorTest {
         IMCCalculator calc = new IMCCalculator(70, 1.75);
         assertEquals(22.86, calc.calculateIMC());
     }
+
+    @Test
+    void testGetIMCClassification() {
+        IMCCalculator calc = new IMCCalculator(20, 1.75);
+        assertEquals("Delgadez severa", calc.getIMCClassification());
+        calc = new IMCCalculator(50, 1.75);
+        assertEquals("Delgadez moderada", calc.getIMCClassification());
+        calc = new IMCCalculator(55, 1.75);
+        assertEquals("Delgadez leve", calc.getIMCClassification());
+        calc = new IMCCalculator(70, 1.75);
+        assertEquals("Peso normal", calc.getIMCClassification());
+        calc = new IMCCalculator(85, 1.75);
+        assertEquals("Sobrepeso", calc.getIMCClassification());
+        calc = new IMCCalculator(95, 1.75);
+        assertEquals("Obesidad leve", calc.getIMCClassification());
+        calc = new IMCCalculator(110, 1.75);
+        assertEquals("Obesidad moderada", calc.getIMCClassification());
+        calc = new IMCCalculator(130, 1.75);
+        assertEquals("Obesidad mórbida", calc.getIMCClassification());
+    }
 }
